8cb42f7e828ac4dd15bc62be4ac6f51609e9ded4
LKNorman <lknorman30@gmail.com>
2020-04-28 22:28:02 -0400

    added favicon, everything shouldwork

--- public/service.worker.js
+++ public/service.worker.js
@@ -12,7 +12,6 @@ const FILES_TO_CACHE = [
 ];
 const CACHE_NAME = "static-cache-v1";
 const DATA_CACHE_NAME = "data-cache-v1";
-// install
 self.addEventListener("install", function (evt) {
   evt.waitUntil(
     caches
@@ -22,11 +21,9 @@ self.addEventListener("install", function (evt) {
         cache
           .addAll(FILES_TO_CACHE)
           .then((result) => {
-            // debugger;
             console.log("result of add all", result);
           })
           .catch((err) => {
-            // debugger;
             console.log("Add all error: ", err);
           });
       })
@@ -35,4 +32,48 @@ self.addEventListener("install", function (evt) {
       })
   );
   self.skipWaiting();
+});
+self.addEventListener("activate", function (evt) {
+  evt.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log("Removing old cache data", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.match(evt.request).then((response) => {
+        return response || fetch(evt.request);
+      });
+    })
+  );
 });
